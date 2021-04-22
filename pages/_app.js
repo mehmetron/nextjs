0cd74c6cf41bd7ee3d47c185fa2233ca2a42ffcc
Austin Langdon <alangdon93@gmail.com>
2021-04-22 20:19:44 +0700

    add theFront components

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -22,11 +19,7 @@ export default function MyApp(props) {
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </React.Fragment>
   );
 }
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * Caution: Consider this file when using NextJS or GatsbyJS
+ * 
+ * You may delete this file and its occurrences from the project filesystem if you are using react-scripts
+ */
+ import React from 'react';
+ import IndexView from 'views/IndexView';
+ import Main from 'layouts/Main';
+ import WithLayout from 'WithLayout';
+ 
+ const IndexPage = (): JSX.Element => {
+   return (
+     <WithLayout
+       component={IndexView}
+       layout={Main}
+     />
+   )
+ };
+ 
+ export default IndexPage;
+ 
